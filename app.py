d04b5d29c9051e1981cc6f995bd606aac2431db6
Pipe <felipesmsoto@gmail.com>
2023-05-24 09:51:54 -0400

    Update app.py
    pivot tables enhanced

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ with st.form("basic_form"):
                         question, st.session_state.qa_retrievals
                     )
                     data.extend(results)
-                st.write(data)
+                
                 st.session_state.data = data
 
             except Exception as e:
@@ -129,8 +129,7 @@ with st.form("basic_form"):
         with st.spinner("Doing Analysis.."):
             try:
                 df = pd.DataFrame(st.session_state.data)
-                st.table(df)
-                #st.table(df.pivot("query", "source_document", "response"))
+                
                 st.table(df.pivot(index="query", columns="source_document", values="response"))
 
             except Exception as e:
